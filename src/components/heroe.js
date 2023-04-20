09ab5eff4076d31e4f69cffd0f85724011371ca9
“Victory <victory23tshimbadi@gmail.com>
2023-04-20 05:29:11 +0100

    refactor heroe

--- src/components/heroe.js
+++ src/components/heroe.js
@@ -3,23 +3,21 @@ import Link from "next/Link"
 
 function Heroe({ image, title, text, label, lien }) {
   return (
-    <div
-      id='heroe'
-      className='px-4 mx-auto md:px-8 lg:max-w-7xl flex flex-row justify-between items-center'
-    >
-      <div className="w-1/2">
-        <h1 className="font-sans text-5xl font-bold mb-4 text-blue-700">{title}</h1>
-        <h4 className="mb-8 font-medium">{text}</h4>
-        <Link href={lien} className="bg-blue-800 hover:bg-blue-500 text-white text-lg font-medium hover:text-white py-2 px-4 border border-white hover:border-transparent rounded">{label}</Link>
-      </div>
-      <Image
-        className='h-auto max-w-full'
-        src={image}
-        height="100"
-        width='350'
-        alt='Hello'
-      />
-    </div>
+    <section class="bg-white dark:bg-gray-900">
+        <div class="grid max-w-screen-xl px-4 py-4 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+            <div class="mr-auto place-self-center lg:col-span-7">
+                <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">{title}</h1>
+                <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">{text}</p>
+                <Link href="#" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+                    Get started
+                    <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </Link> 
+            </div>
+            <div class="hidden lg:mt-0 lg:col-span-5 lg:flex lg:flex-row lg:justify-right">
+                <Image src={image} width={363} height={200} alt="mockup"/>
+            </div>                
+        </div>
+    </section>
   );
 }
 
