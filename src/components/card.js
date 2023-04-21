0a25f9bdec0f1058f6bbf35cda37e01d4e1f7d33
“Victory <victory23tshimbadi@gmail.com>
2023-04-21 14:46:31 +0100

    create card

--- src/components/card.js
+++ src/components/card.js
@@ -1,13 +1,17 @@
+import Link from 'next/link'
+import Image from "next/image"
 
-const Card = () => {
+const Card = ({lien, title, text, image, width}) => {
   return (
-<a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-    <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/docs/images/blog/image-4.jpg" alt="">
-    <div class="flex flex-col justify-between p-4 leading-normal">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-    </div>
-</a>
+    <Link href={lien} className={`${width} flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700`}>
+        <div className="w-full">
+            <Image src={image} alt="image"/>
+        </div>
+        <div className="w-full flex flex-row justify-between p-4 leading-normal">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{text}</p>
+        </div>
+    </Link>
   )
 }
 
